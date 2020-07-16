f13a0dd39ef6d7ed130e9ba25095112bb7d3241b
Terry Thompson <terrylthompsonintx@gmail.com>
2020-07-16 08:46:23 -0500

    moment installed

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 const Comment = require('./Comment');
+const moment = require('moment');
 
 const PizzaSchema = new Schema(
     {
@@ -12,6 +13,7 @@ const PizzaSchema = new Schema(
       createdAt: {
         type: Date,
         default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
       },
       size: {
         type: String,
@@ -28,6 +30,7 @@ const PizzaSchema = new Schema(
     {
       toJSON: {
         virtuals: true,
+        getters: true
       },
       id: false
     }
